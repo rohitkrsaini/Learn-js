f5f41b7d94068c6261dd9fadcd6f7b4510f6372f
ROHIT SAINI <rohit.rksaini28@gmail.com>
2026-02-11 22:40:35 +0530

    import & export

---
+++ function.js
@@ -0,0 +1,19 @@
+//! named 
+function greet(){
+    console.log("i am function");
+    
+}
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+export{
+    greet,sum
+}
+
+//! default
+
+// export function mul(c,d){
+//     return c*d
+// }
---
+++ script2.js
@@ -0,0 +1,11 @@
+//! named
+import { greet, sum as add } from "./function.js";
+
+greet()
+add(5,3)
+
+//! defuilt
+
+// import mul from"./function.js"
+// mul(5,3)
+// console.log(mul);
