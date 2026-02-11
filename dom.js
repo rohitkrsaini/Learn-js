5a418e16b6b27a21b80c856d8383a6944af260bc
ROHIT SAINI <rohit.rksaini28@gmail.com>
2026-02-11 21:55:33 +0530

    Evants

--- dom.js
+++ dom.js
@@ -57,3 +57,159 @@ console.log(ele5);
 
 // HTML COLLECTION IS CALLED LIVE COLLECTION
 // NODE LIST IS CALLED STATIC COLLECTION
+
+//! Events
+
+
+//! EXAMPLE OF STATIC AND LIVE COLLECTION
+// const collection = document.getElementsByTagName("div");
+// const list = document.querySelectorAll("div");
+
+// console.log("HTMLCollection -->", collection.length); // 3
+// console.log("NodeList -->", list.length); // 3
+
+// // creating HTML Element dynamically
+// for (let i = 0; i < 5; i++) {
+//   const divTag = document.createElement("div");
+//   divTag.textContent = "div 4";
+//   console.log(divTag); // <div>div 4</div>
+//   document.body.appendChild(divTag);
+// }
+
+// console.log("HTMLCollection -->", collection.length); // 8 (LIVE)
+// console.log("NodeList -->", list.length); // 3 (STATIC)
+
+// //! DOM MANIPULATION
+// const mainContainer = document.querySelector("#table-container");
+// const tableTag = document.createElement("table");
+
+// const trTag1 = document.createElement("tr");
+// const trTag2 = document.createElement("tr");
+// const trTag3 = document.createElement("tr");
+
+// const thTag1 = document.createElement("th");
+// const thTag2 = document.createElement("th");
+// const thTag3 = document.createElement("th");
+
+// const tdTag1 = document.createElement("td");
+// const tdTag2 = document.createElement("td");
+// const tdTag3 = document.createElement("td");
+// const tdTag4 = document.createElement("td");
+// const tdTag5 = document.createElement("td");
+// const tdTag6 = document.createElement("td");
+
+// //! adding attributes
+// tableTag.border = "1";
+// tableTag.cellSpacing = "0";
+// tableTag.cellPadding = "5";
+
+// //! adding textContent
+// thTag1.textContent = "ID";
+// thTag2.textContent = "FNAME";
+// thTag3.textContent = "LNAME";
+
+// tdTag1.textContent = "1";
+// tdTag2.textContent = "John";
+// tdTag3.textContent = "Doe";
+// tdTag4.textContent = "2";
+// tdTag5.textContent = "Jane";
+// tdTag6.textContent = "Doe";
+
+// //! append as a child
+// trTag1.append(thTag1, thTag2, thTag3);
+// trTag2.append(tdTag1, tdTag2, tdTag3);
+// trTag3.append(tdTag4, tdTag5, tdTag6);
+
+// tableTag.append(trTag1, trTag2, trTag3);
+
+// mainContainer.append(tableTag);
+
+//! EVENTS :  ACTIONS PERFORMED BY THE USER
+
+//! MOUSE EVENTS
+// onclick
+function singleClick() {
+  console.log("single clicked");
+}
+
+// ondblclick
+function doubleClick() {
+  console.log("Double CLick");
+}
+
+// onmouseenter
+function cursorEnter() {
+  console.log("cursor entered");
+}
+
+// onmouseleave
+function cursorExit() {
+  console.log("cursor left");
+}
+
+// onmousemove
+function cursorMove() {
+  console.log("cursor Moved");
+}
+
+//! KEYBOARD EVENTS
+// onkeydown
+function keyPressed() {
+  console.log("key is Pressed");
+}
+
+// onkeyup
+function keyReleased() {
+  console.log("key is Released");
+}
+
+// onchange
+function inputChanged() {
+  console.log("input is changed");
+}
+
+//! FORM EVENTS
+// onsubmit <-- always used in form tag
+function handleForm1(e) {
+  e.preventDefault(); // stops page reload
+
+  //! WAY 1
+  //   let formData = {
+  //     email: e.target[0].value,
+  //     password: e.target[1].value,
+  //   };
+  //   console.log(formData);
+
+  //! WAY 2
+  let email = document.querySelector("#email");
+  let password = document.querySelector("#password");
+
+  let formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+}
+
+//! WINDOW EVENTS
+// onload{
+function pageLoad() {
+  console.log("Page is loaded");
+}
+
+// onscroll
+function pageScroll() {
+  console.log("Page scrolled");
+}
+
+// ! addEventListener( "event" ,  callbackFunc , useCapture )
+
+const h1Tag = document.createElement("h1");
+h1Tag.textContent = "Learn addEventListener";
+
+h1Tag.addEventListener("click", () => {
+  console.log("H1 clicked");
+  h1Tag.style.backgroundColor = "red";
+});
+
+document.body.append(h1Tag);
